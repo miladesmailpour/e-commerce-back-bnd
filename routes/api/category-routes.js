5f5cb71876411d1c830a6033b5d9f75300b8a3c2
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-23 16:35:34 -0400

    get route added the category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,40 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryRoutes = await Category.findAll({ 
+      include: [
+        { model: Product },
+      ]
+    });
+    res.status(200).json(categoryRoutes);
+  } 
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [
+        { model: Product },
+      ]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: `category with id:${req.params.id} NOT exist`});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } 
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
